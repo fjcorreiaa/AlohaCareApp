722a0f1b206d90a655adcd0e9adce1772a6f4943
Fernanda Correia <75763484+fjcorreiaa@users.noreply.github.com>
2024-12-11 01:07:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("Index.html")
 
 if __name__ == "__main__":
     app.run()
